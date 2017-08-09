1a218ace02dedcff686bfe075b0988d890d9cb7b
Trent Killinger <trentkillinger@gmail.com>
2017-08-09 10:26:32 -0400

    Fixed Database.php

--- Website/php/database.php
+++ Website/php/database.php
@@ -1,7 +1,7 @@
 <?php
     class Database{
         public static function prepare($sql, $values) {
-            $pdo = new PDO("mysql:host=localhost;dbname=tlkillin", "tlkillin", "507072");
+            $pdo = new PDO("mysql:host=localhost;dbname=dbname", "dbname", "password");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $query = $pdo->prepare($sql);
             $query->execute($values);
